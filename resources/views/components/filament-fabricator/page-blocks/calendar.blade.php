e00748aa302190bc3bb7c255d4bc867f4fe27c7d
Greg Ross <gregross.dev@gmail.com>
2024-10-29 11:44:26 -0400

    calendar block

---
+++ resources/views/components/filament-fabricator/page-blocks/calendar.blade.php
@@ -0,0 +1,15 @@
+@aware(['page'])
+<div class="px-4 py-4 md:py-8">
+    <div class="max-w-7xl mx-auto">
+
+        {!! $calendarContent !!}
+        {!! $calendar !!}
+
+        {!! $eventContent !!}
+        {!! $eventTitle !!}
+        {!! $eventDate !!}
+        {!! $eventLocation !!}
+        {!! $eventFlyer !!}
+
+    </div>
+</div>
